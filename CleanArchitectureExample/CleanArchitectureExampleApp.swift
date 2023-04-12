5f47ac5f8c358a97583aa9ac2c340a352d356d6b
danielios-hub <daniel.ios.dgp@gmail.com>
2023-04-12 18:22:34 +0200

    Initial Commit

---
+++ CleanArchitectureExample/CleanArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureExampleApp.swift
+//  CleanArchitectureExample
+//
+//  Created by Daniel Gallego Peralta on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
